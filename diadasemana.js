e741b9cf1e5369579a7a97488bc26e3c63ea9127
LuanPSX18 <luanplaystation@gmail.com>
2024-04-18 19:57:14 -0300

    primeiro exercício em javascript

---
+++ diadasemana.js
@@ -0,0 +1,29 @@
+//Desafio dia da semana em Javascript, minha primeira tentativa:
+
+/* alert('Olá bom dia ');
+let seg = 'segunda';
+let ter = 'terça';
+let qua = 'quarta';
+let qui = 'quinta';
+let sex = 'sexta';
+let sab = 'sábado';
+let dom = 'domingo';
+
+let dia = prompt('Qual é o dia da semana? ');
+if (dia == sab, dom) {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!')
+}     */
+
+
+//Código do instrutor: 
+
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
